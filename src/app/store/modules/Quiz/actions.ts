7ac55f3aa3b6d6586e1faa5b64e18b7fa95aaa21
Alejandro Gaspar <alejandro.gaspar@lucidfusion.com>
2020-06-04 14:46:30 -0700

    fix(GA) using ga instead _gaq [deploy]

--- src/app/store/modules/Quiz/actions.ts
+++ src/app/store/modules/Quiz/actions.ts
@@ -2,8 +2,11 @@ import { defineActions } from 'direct-vuex';
 
 export default defineActions({
   nextStep({ commit }, step): void {
-    const _gaq = window._gaq || [];
-    _gaq.push([`_trackEvent`, `Step`, step]);
+    try {
+      window.ga(`send`, `event`, `Quiz`, `Changing Step`, `Step`, step);
+    } catch (error) {
+      console.error(`Ga error: `, error);
+    }
     commit(`setStep`, step);
   },
   back({ commit }, step): void {
