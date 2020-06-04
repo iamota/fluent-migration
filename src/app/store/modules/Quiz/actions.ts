b0ebde908b964725e806b4266a0581e8f88f289a
Alejandro Gaspar <alejandro.gaspar@lucidfusion.com>
2020-06-04 12:33:04 -0700

    feat(Quiz) change url with the steps [deploy]

--- src/app/store/modules/Quiz/actions.ts
+++ src/app/store/modules/Quiz/actions.ts
@@ -2,6 +2,8 @@ import { defineActions } from 'direct-vuex';
 
 export default defineActions({
   nextStep({ commit }, step): void {
+    const new_url = `${window.location.protocol}//${window.location.host}${window.location.pathname}#${step}`;    
+    window.history.pushState({ path: new_url }, ``, new_url);
     commit(`setStep`, step);
   },
   back({ commit }, step): void {
