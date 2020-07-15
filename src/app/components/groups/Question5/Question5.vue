aa082e5f686f03238240a5bdc22fd8bd293d71cd
Alejandro Gaspar <alejandro.gaspar@lucidfusion.com>
2020-07-15 13:49:24 -0700

    fix(bugs) [deploy]

--- src/app/components/groups/Question5/Question5.vue
+++ src/app/components/groups/Question5/Question5.vue
@@ -3,7 +3,7 @@
     <div class="Quiz__content">
       <h3>How many people live in your household, including you?</h3>
       <h4>Adults</h4>
-      <Grid desktop="4" tablet="4" mobile="3">
+      <Grid desktop="4" tablet="4" mobile="2">
         <Radio 
           name="household_adults" 
           label="1" 
@@ -31,7 +31,7 @@
         />
       </Grid>
       <h4>Children (under age 18)</h4>
-      <Grid desktop="4" tablet="4" mobile="3">
+      <Grid desktop="4" tablet="4" mobile="2">
         <Radio 
           name="household_children" 
           label="None" 
