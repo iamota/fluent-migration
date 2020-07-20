ea068e90ff62a6e7613a31fc1f4fd96b48604f48
Li Zhang <li.zhang@lucidfusion.com>
2020-07-20 14:40:23 -0700

    fix(Question15): updates text

--- src/app/components/groups/Question15/Question15.vue
+++ src/app/components/groups/Question15/Question15.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="Quiz__slide Question15">
     <div class="Quiz__content">
-      <h3 v-if="isMyself">Did you contact a medical professional over the phone or internet (not in person)?</h3>
-      <h3 v-if="!isMyself">Did your family member contact a medical professional over the phone or internet (not in person)?</h3>
+      <h3 v-if="isMyself">Did you contact a medical professional over the phone or internet (telemedicine, email, video, patient portal, etc)?</h3>
+      <h3 v-if="!isMyself">Did your family member contact a medical professional over the phone or internet (telemedicine, email, video, patient portal, etc)?</h3>
       
       <div class="Radio__group">
         <Radio 
