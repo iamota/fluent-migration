1d725e350e4f89d570b1be9af0e5ea6fac486c58
Kieran Reynolds <kieran.reynolds@vmny-kieran.reynolds>
2020-09-11 10:07:34 -0700

    fix(cms): Finished including CMS core js forks
    # Conflicts:
    #	package-lock.json

---
+++ src/cms/scripts/core/getMedia.js
@@ -0,0 +1,4 @@
+import shoppableWrapper from 'infinite/cms/scripts/core/shoppableWrapper';
+import generateBackground from 'infinite/cms/scripts/core/CMS-Background';
+
+export default (data, is_bg_media, module_name, module_index, submodule_index) => { return null; };
