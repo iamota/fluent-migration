48b012f544d06e762300a17bd1c457914cee21dc
Kieran Reynolds <kieran.reynolds@vmny-kieran.reynolds>
2020-09-10 18:43:51 -0700

    feat: Build CMS core hooked functions

---
+++ src/cms/scripts/core/getElements.js
@@ -0,0 +1,6 @@
+import shoppableWrapper from 'infinite/cms/scripts/core/shoppableWrapper';
+import getBlockquote from 'infinite/cms/scripts/core/CMS-Blockquote';
+import getHeadings from 'infinite/cms/scripts/core/getHeadings';
+import getButtons from 'infinite/cms/scripts/core/CMS-Button';
+
+export default (module_name, index) => { return null; };
