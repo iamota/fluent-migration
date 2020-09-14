c7ef40c1ac53e00dccf8438377189719715f381f
Fernando Herrera <fernando.herrera@lucidfusion.com>
2020-09-14 16:21:50 -0700

    feat(Quiz): Recommended product dynamically pulls based on API.

--- src/app/store/modules/Quiz/state.ts
+++ src/app/store/modules/Quiz/state.ts
@@ -2,7 +2,7 @@ declare global {
   export namespace Quiz {
     interface State {
       step: string;
-      product_id: number | unknown;
+      product_id: number;
       product_handle: string;
       kit_title: string;
       kit_body: string;
@@ -14,7 +14,7 @@ const state: Quiz.State = {
   step: `starter`,
   kit_body: `You have the flu. <br/>We recommend the Adult Flue Relief Pack.`,
   kit_title: `Based on your symptoms...`,
-  product_id: 5574461685917,
+  product_id: 0,
   product_handle: `dev-test-product-do-not-delete`,
 };
 
