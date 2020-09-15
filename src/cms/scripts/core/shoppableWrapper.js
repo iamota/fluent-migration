4d0246afd9fd69fe5a4f3ae6240fcd3c7e40774e
Li Zhang <li.zhang@lucidfusion.com>
2020-09-15 09:41:53 -0700

    feat(ShoppableImage): forks shoppableWrapper to add background-size.

--- src/cms/scripts/core/shoppableWrapper.js
+++ src/cms/scripts/core/shoppableWrapper.js
@@ -1,3 +1,48 @@
 import _ from 'lodash-es';
 
-export default (content, is_bg_image, height) => { return null; };
+export default (content, is_bg_image, height) => {
+  let shoppable = '';
+  let max_height = '';
+  let background = '';
+  let background_size = '';
+  // console.log("CONTENT>>>", content);
+
+  if (content.shoppable_image.is_shoppable && (content.shoppable_image.shoppable_products.length > 0)) {
+    const shoppable_products = _.map(content.shoppable_image.shoppable_products, (product) => {
+      if (product.variant) {
+        return `${product.handle}:${product.variant}`;
+      }
+      return `${product.handle}`;
+    });
+    shoppable = `data-shoppable-image="${shoppable_products}"`;
+  }
+
+  if (height) {
+    max_height = is_bg_image 
+      ? `height: ${height}px; ` 
+      : `max-height: ${height}px; `;
+  }
+
+  if (is_bg_image) {
+    background = `background-image: url('${content.image}'); `;
+    background_size = `background-size: ${content.image_size}; `;
+  }
+
+  // intend to have bg_image use a bg on div but has been implemented differently elsewhere and will need to rework
+
+  const html = `
+    <div 
+      class="ShoppableImage ${is_bg_image ? 'ShoppableImage--full' : ''}" 
+      style="${max_height}${background}${background_size}" 
+      ${shoppable}
+    >
+      <img 
+        data-shoppable-source-image 
+        class="ShoppableImage__Image" 
+        src="${content.image}" 
+        alt="${content.alt_text}" 
+      />
+    </div>
+  `;
+  return html;
+ };
