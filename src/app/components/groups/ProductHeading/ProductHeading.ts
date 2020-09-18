9e377411c479c55b56cbf0835f9b2727cfa161bf
Kieran Reynolds <kieran.reynolds@vmny-kieran.reynolds>
2020-09-18 14:34:45 -0700

    fix(productHeading): Hide preheading and price on unauth assessment product

--- src/app/components/groups/ProductHeading/ProductHeading.ts
+++ src/app/components/groups/ProductHeading/ProductHeading.ts
@@ -1,4 +1,26 @@
 import { ProductHeading } from '@INF/groups/ProductHeading/';
   
+interface KitData {
+  gradient_class: string;
+  image_src_desktop: string | unknown;
+  image_src_mobile: string | unknown;
+  shopify_product_handle: string;
+  product: GenericObject;
+}
+
 export default ProductHeading.extend({
+  computed: {
+    assessment(): boolean {
+      const tags: string[] = this.product_data.tags;
+      
+      if (tags.length === 0) { return false; }      
+
+      return tags.map((tag) => tag.toLowerCase()).indexOf(`assessment`) > -1;
+    },
+    authorized(): boolean {
+      const kit_information = localStorage.getItem(`kit_data`) as string;
+      const kit_data: KitData = JSON.parse(kit_information);
+      return this.assessment ? this.product_data.handle === kit_data.shopify_product_handle : true;
+    },
+  },
 });
--- src/app/components/groups/ProductHeading/ProductHeading.vue
+++ src/app/components/groups/ProductHeading/ProductHeading.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="ProductHeading" :class="{[`ProductHeading--${breakpoint}`]: true}">
-    <h5 class="ProductHeading__pre-title">{{ product.product_data.type }}</h5>
+    <h5 v-if="authorized" class="ProductHeading__pre-title">{{ product.product_data.type }}</h5>
     <h1 class="ProductHeading__title">{{ product.product_data.title }}</h1>
     <Price 
+      v-if="authorized"
       class="ProductHeading__price"
       :price="getters.price"
       :compare_at_price="getters.compare_at_price"
