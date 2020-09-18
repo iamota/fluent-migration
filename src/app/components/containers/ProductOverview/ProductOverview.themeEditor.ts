e69d6d2ca5e8154144faba1e3c3a842de469065f
Fernando Herrera <fernando.herrera@lucidfusion.com>
2020-09-18 09:56:08 -0700

    fix(PDP): map variables correcly to what's in the section settings..

---
+++ src/app/components/containers/ProductOverview/ProductOverview.themeEditor.ts
@@ -0,0 +1,16 @@
+const methods: SectionEditorMethods = {
+  $sectionSelect(e) {
+    
+  },
+  $sectionDeselect(e) {
+
+  },
+  $blockSelect(e) {
+
+  },
+  $blockDeselect(e) {
+
+  },
+};
+
+export default methods;
--- src/app/components/containers/ProductOverview/ProductOverview.ts
+++ src/app/components/containers/ProductOverview/ProductOverview.ts
@@ -1,5 +1,20 @@
 import { ProductOverview } from '@INF/containers/ProductOverview';
+import { default_props, default_css_variables } from './ProductOverview.defaults';
+import themeEditor from './ProductOverview.themeEditor';
   
 export default ProductOverview.extend({
-
+  props: {
+    ...default_props,
+  },
+  computed: {
+    cssVariables(): object {
+      return {
+        ...default_css_variables(this),
+        // '--my-custom-css-variable': this.my_custom_prop,
+      };
+    },
+  },
+  methods: {
+    ...themeEditor,
+  },
 });
