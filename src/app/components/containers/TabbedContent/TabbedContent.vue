4ad3d4bbe6217def4faedca8133ed635abd6e87d
Kieran Reynolds <kieran.reynolds@vmny-kieran.reynolds>
2020-09-21 14:33:54 -0700

    fix(tabbedContent): Does not render if no tabs

--- src/app/components/containers/TabbedContent/TabbedContent.vue
+++ src/app/components/containers/TabbedContent/TabbedContent.vue
@@ -1,40 +1,42 @@
 <template>
   <Section v-bind="$props" :css_variables="cssVariables">
-    <Carousel
-      :id="`TabbedCarousel`"
-      class="TabbedContent__tabs"
-      v-bind="mobile_props"
-      cell_align="left"
-      as_nav_for="#TabbedCarousel--content"
-      :draggable="true"
-    >
-      <Slide
-        v-for="(title, index) in tabTitles"
-        :key="index"
-        class="TabbedContent__tab"
+    <template v-if="tabs.length > 0">
+      <Carousel
+        :id="`TabbedCarousel`"
+        class="TabbedContent__tabs"
+        v-bind="mobile_props"
+        cell_align="left"
+        as_nav_for="#TabbedCarousel--content"
+        :draggable="true"
       >
-        <div>
-          <span>{{ title }}</span>
-        </div>
-      </Slide>
-    </Carousel>
-    <Carousel
-      :id="`TabbedCarousel--content`"
-      ref="TabbedCarouselContent"
-      class="TabbedContent__content"
-      v-bind="mobile_props"
-      :fade="true"
-    >
-      <Slide
-        v-for="(tab_data, index) in tabs"
-        :key="index"
-        class="TabbedContent__body"
+        <Slide
+          v-for="(title, index) in tabTitles"
+          :key="index"
+          class="TabbedContent__tab"
+        >
+          <div>
+            <span>{{ title }}</span>
+          </div>
+        </Slide>
+      </Carousel>
+      <Carousel
+        :id="`TabbedCarousel--content`"
+        ref="TabbedCarouselContent"
+        class="TabbedContent__content"
+        v-bind="mobile_props"
+        :fade="true"
       >
-        <div
-          v-html="tab_data.tab_content"
-        />
-      </Slide>
-    </Carousel>
+        <Slide
+          v-for="(tab_data, index) in tabs"
+          :key="index"
+          class="TabbedContent__body"
+        >
+          <div
+            v-html="tab_data.tab_content"
+          />
+        </Slide>
+      </Carousel>
+    </template>
   </Section>
 </template>
 
