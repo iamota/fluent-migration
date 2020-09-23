06939bfaab2fcec597a40f4d83512757d9f827ad
Li Zhang <li.zhang@lucidfusion.com>
2020-09-22 17:38:30 -0700

    test(BH#301) [deploy]

--- src/app/legacy/sections/INF-SliderCart/openCart.js
+++ src/app/legacy/sections/INF-SliderCart/openCart.js
@@ -33,6 +33,9 @@ export default function(toggle) {
   window.focusedContainer = $('[data-cart]').get(0);
   $('[data-state]').addClass('State--cart-open');
   const $sticky_checkout = $('[data-sticky-checkout]');
+  $sticky_checkout.css({
+    top: 0,
+  });
   setTimeout(() => {
     // Hextom FSB is 44 px tall; Set 'top' to fix Safari's 'interact' bug
     $sticky_checkout.css({
