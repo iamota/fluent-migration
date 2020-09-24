5ecfe44ba6bb9565e614059cf15791ea7a0f4272
Fernando Herrera <fernando.herrera@lucidfusion.com>
2020-09-24 11:10:56 -0700

    fix(Price): adjust layout and styles for pricing. BH#305

--- src/app/components/units/Price/Price.vue
+++ src/app/components/units/Price/Price.vue
@@ -1,22 +1,29 @@
 <template>
   <div class="Price">
-    <span class="Price__sale" v-if="parseFloat(compare_at_price) > parseFloat(price)">
-      <template v-if="price_varies">
-        {{ 'products.product.on_sale_from_html' | t({ price: formatted_min_price }) }}
-        <span v-if="show_currency" class="Price__currency">
-          {{ currency }}
+    
+    <template>      
+      <template v-if="parseFloat(compare_at_price) > parseFloat(price)">
+        <span class="visually-hidden">
+          {{ 'products.product.regular_price' | t }}        
         </span>
+        <s class="Price__compare-at">{{ compare_at_price | money }}</s>
       </template>
-      <template v-else>
-        {{ 'products.product.on_sale' | t }}
-        {{ price | money }}
-        <span v-if="show_currency" class="Price__currency">
-          {{ currency }}
-        </span>
-      </template>
-    </span>
-    <template v-else>
-      <template v-if="price_varies">
+      <span class="Price__sale" v-if="parseFloat(compare_at_price) > parseFloat(price)">
+        <template v-if="price_varies">
+          {{ 'products.product.on_sale_from_html' | t({ price: formatted_min_price }) }}
+          <span v-if="show_currency" class="Price__currency">
+            {{ currency }}
+          </span>
+        </template>
+        <template v-else>
+          <!-- {{ 'products.product.on_sale' | t }} -->
+          {{ price | money }}
+          <span v-if="show_currency" class="Price__currency">
+            {{ currency }}
+          </span>
+        </template>
+      </span>
+      <template v-else-if="price_varies">
         {{ 'products.product.from_text_html' | t({ price: formatted_min_price }) }}
         <span v-if="show_currency" class="Price__currency">
           {{ currency }}
@@ -33,12 +40,6 @@
           {{ currency }}
         </span>
       </template>
-    </template>
-    <template v-if="parseFloat(compare_at_price) > parseFloat(price)">
-      <span class="visually-hidden">
-        {{ 'products.product.regular_price' | t }}        
-      </span>
-      <s class="Price__compare-at">{{ compare_at_price | money }}</s>
     </template>
     <template v-if="!availability">
       <span class="Price__sold-out">{{ 'products.product.sold_out' | t }}</span>
