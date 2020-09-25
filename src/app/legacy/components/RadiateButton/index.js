7c4c0324992672dc5233cc7b397e72e729a33ada
Li Zhang <li.zhang@lucidfusion.com>
2020-09-25 10:24:40 -0700

    [deploy]

--- src/app/legacy/components/RadiateButton/index.js
+++ src/app/legacy/components/RadiateButton/index.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 
-// Lame fix for Safari 14.0. The browser occasionally fails to position the radiating pseudo element correctly on page load, hence the SetTimeout.
+// Lame fix for Safari 14.0. The browser occasionally fails to position the radiating pseudo element correctly on page load, hence the SetTimeout. 
 export default () => {
   const $radiate_buttons = $(`[data-button-radiate]`);
   if ($radiate_buttons.length === 0) { return; }
